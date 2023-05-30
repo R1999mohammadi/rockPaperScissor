41e42d2033cd685fee215d3ff11ad26c3a4e86f4
Rafi <rafi1999mohammadi@gmail.com>
2023-05-30 02:12:59 -0400

    html is completed successfully

--- index.js
+++ index.js
@@ -1,40 +1,42 @@
 
-function getComputerChoice() {
-    let myArray = ['rock', 'paper','scissors'];
-    let randomNumber = Math.floor(Math.random()* 3);
+// function getComputerChoice() {
+//     let myArray = ['rock', 'paper','scissors'];
+//     let randomNumber = Math.floor(Math.random()* 3);
     
-    return myArray[randomNumber];
-}
+//     return myArray[randomNumber];
+// }
 
 
 
-function playRound(uSelect, cSelction) {
+// function playRound(uSelect, cSelction) {
 
-        let userSelection = uSelect.toLowerCase();
-        let computerSelection = cSelction;
-        if(userSelection == 'rock' && computerSelection =="scissors") {
-            return `You won! Rock beats Scissors`;
-        } else if (userSelection == 'scissors' && computerSelection =="paper") {
-            return `You won! scissors beats paper`;
-        } else if (userSelection =="paper" && computerSelection =="rock") {
-            return `Your won! paper beats Rock`;
-        } else if (userSelection == computerSelection) {
-            return `Tied! ${userSelection} can not beat ${computerSelection}`;
-        }
-        return `You lost ${computerSelection} beats ${userSelection}`;
+//         let userSelection = uSelect.toLowerCase();
+//         let computerSelection = cSelction;
+//         if(userSelection == 'rock' && computerSelection =="scissors") {
+//             return `You won! Rock beats Scissors`;
+//         } else if (userSelection == 'scissors' && computerSelection =="paper") {
+//             return `You won! scissors beats paper`;
+//         } else if (userSelection =="paper" && computerSelection =="rock") {
+//             return `Your won! paper beats Rock`;
+//         } else if (userSelection == computerSelection) {
+//             return `Tied! ${userSelection} can not beat ${computerSelection}`;
+//         }
+//         return `You lost ${computerSelection} beats ${userSelection}`;
         
-}
+// }
 
-function game() {
+// function game() {
 
-            let counter = 0;
-            while (counter <5) {
-                let userInput = prompt("Rock,paper,scissors");
-               console.log( playRound(userInput,getComputerChoice()));
-                counter +=1;
+//             let counter = 0;
+//             while (counter <5) {
+//                 let userInput = prompt("Rock,paper,scissors");
+//                console.log( playRound(userInput,getComputerChoice()));
+//                 counter +=1;
 
-            }
+//             }
 
-}
+// }
 
-game();
+// game();
+
+
